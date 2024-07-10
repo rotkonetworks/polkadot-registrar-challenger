15fc0b8be7f17c939ce4c79b9e97138d6a8dfdc9
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-07-10 17:31:01 +0200

    Re-enable watcher communication

--- src/lib.rs
+++ src/lib.rs
@@ -128,10 +128,10 @@ pub async fn run() -> Result<()> {
             ).await?;
         }
 
-        // info!("Connecting to watchers");
-        // let watchers = config.watchers.clone();
-        // let dn_config = config.display_name.clone();
-        // watcher::open_connections(db, watchers, dn_config).await?;
+        info!("Connecting to watchers");
+        let watchers = config.watchers.clone();
+        let dn_config = config.display_name.clone();
+        watcher::open_connections(db, watchers, dn_config).await?;
     }
 
     info!("Setup completed");
