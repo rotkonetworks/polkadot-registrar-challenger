cf0e84bc720a2078acc0c96d42d64bbb79a6ae0a
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-07-27 21:06:25 +0200

    Rip out watcher/blockchain related code
    It needs to be significantly reworked.

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,11 @@ extern crate tracing;
 extern crate anyhow;
 #[macro_use]
 extern crate serde;
-extern crate async_trait;
 
 mod matrix;
-mod registrar;
-
-use registrar::Database;
-use registrar::ChainName;
+use matrix::Nickname;
 
 use actix::clock::sleep;
-use matrix::Nickname;
 use std::fs;
 use std::time::Duration;
 
@@ -23,8 +18,7 @@ pub type Result<T> = std::result::Result<T, anyhow::Error>;
 #[serde(rename_all = "snake_case")]
 struct Config {
     pub log_level: LogLevel,
-    pub db: DatabaseConfig,
-    pub listener: Option<ListenerConfig>,
+    pub matrix: MatrixConfig,
 }
 
 #[derive(Debug, Deserialize)]
@@ -49,28 +43,6 @@ impl LogLevel {
     }
 }
 
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")]
-struct DatabaseConfig {
-    pub uri: String,
-    pub name: String,
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename_all = "snake_case")]
-struct ListenerConfig {
-    pub watchers: Vec<WatcherConfig>,
-    pub matrix: MatrixConfig,
-    pub display_name: DisplayNameConfig,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct WatcherConfig {
-    pub network: ChainName,
-    pub endpoint: String,
-}
-
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "snake_case")]
 struct MatrixConfig {
@@ -80,12 +52,6 @@ struct MatrixConfig {
     pub password: String,
     pub security_key: String,
     pub admins: Option<Vec<Nickname>>,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct DisplayNameConfig {
-    pub enabled: bool,
-    pub limit: f64,
 }
 
 fn open_config() -> Result<Config> {
@@ -102,39 +68,22 @@ fn open_config() -> Result<Config> {
 
 pub async fn run() -> Result<()> {
     let config = open_config()?;
-    let db_config = config.db;
 
     tracing_subscriber::fmt()
         .with_env_filter(format!("system={}", config.log_level.as_str()))
         .init();
 
-    info!("Connecting to the database");
-    let db = Database::new(&db_config.uri, &db_config.name).await?;
-    db.connectivity_check().await?;
-
-    if let Some(config) = config.listener {
-        if config.matrix.enabled {
-            info!("Starting Matrix bot");
-            let config = config.matrix;
-            matrix::start_bot(
-                db.clone(),
-                matrix::BotConfig {
-                    homeserver: &config.homeserver,
-                    username: &config.username,
-                    password: &config.password,
-                    security_key: &config.security_key,
-                    admins: config.admins.unwrap_or_default(),
-                }
-            ).await?;
-        }
-
-        // info!("Connecting to watchers");
-        // let watchers = config.watchers.clone();
-        // let dn_config = config.display_name.clone();
-        // registrar::connect_to_watchers(db, watchers, dn_config).await?;
+    if config.matrix.enabled {
+        info!("Starting Matrix bot");
+        let config = config.matrix;
+        matrix::start_bot(matrix::BotConfig {
+            homeserver: &config.homeserver,
+            username: &config.username,
+            password: &config.password,
+            security_key: &config.security_key,
+            admins: config.admins.unwrap_or_default(),
+        }).await?;
     }
-
-    info!("Setup completed");
 
     loop {
         sleep(Duration::from_secs(u64::MAX)).await;
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,21 +1,17 @@
 use crate::Result;
-use crate::registrar::{ChainAddress, ChainName, Database, ExternalMessage, ExternalMessageType, IdentityContext, RawFieldName, Response, Timestamp};
 
 use matrix_sdk::ruma::events::room::message::MessageType;
 use matrix_sdk::ruma::events::room::message::OriginalSyncRoomMessageEvent;
-use matrix_sdk::ruma::events::room::message::RoomMessageEventContent;
 use matrix_sdk::ruma::events::room::member::StrippedRoomMemberEvent;
 use matrix_sdk::Client;
 use matrix_sdk::config::SyncSettings;
 use matrix_sdk::room::Room;
-use matrix_sdk::event_handler::Ctx;
 use matrix_sdk::ruma::events::AnySyncMessageLikeEvent;
 use matrix_sdk::encryption::{BackupDownloadStrategy, EncryptionSettings};
 use matrix_sdk::matrix_auth::MatrixSession;
 use tokio::time::{sleep, Duration};
 
 use std::path::Path;
-use std::str::FromStr;
 
 const STATE_DIR: &str = "/tmp/matrix";
 
@@ -31,12 +27,7 @@ pub struct BotConfig<'a> {
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct Nickname(String);
 
-#[derive(Debug, Clone)]
-struct BotContext {
-    db: Database,
-}
-
-pub async fn start_bot<'a>(db: Database, cfg: BotConfig<'a>) -> Result<()> {
+pub async fn start_bot<'a>(cfg: BotConfig<'a>) -> Result<()> {
     let state_dir = Path::new(STATE_DIR);
     let session_path = state_dir.join("session.json");
 
@@ -83,7 +74,6 @@ pub async fn start_bot<'a>(db: Database, cfg: BotConfig<'a>) -> Result<()> {
     client.add_event_handler(on_any_message_like_event);
     client.add_event_handler(on_stripped_state_member);
     client.add_event_handler(on_room_message);
-    client.add_event_handler_context(BotContext { db });
 
     // Import secrets. This should enable cross-signing and verify the session.
     info!("Importing secrets");
@@ -141,153 +131,8 @@ async fn on_stripped_state_member(
     });
 }
 
-async fn on_room_message(e: OriginalSyncRoomMessageEvent, room: Room, ctx: Ctx<BotContext>) {
-    let MessageType::Text(text) = e.content.msgtype else {
-        return;
+async fn on_room_message(e: OriginalSyncRoomMessageEvent, _room: Room) {
+    if let MessageType::Text(text) = e.content.msgtype {
+        info!("Received message from {}:\n\t\n\t{}\n", e.sender, text.body);
     };
-
-    info!("Received message from {}:\n\t\n\t{}\n", e.sender, text.body);
-
-    if let Ok(cmd) = Command::from_str(&text.body) {
-        info!("Executing command {:#?}", cmd);
-
-        let res = execute_command(&ctx.db, cmd).await;
-        room
-            .send(RoomMessageEventContent::text_plain(res.to_string()))
-            .await
-            .unwrap();
-   } else {
-        info!("Verifying message");
-
-        let msg = ExternalMessage {
-            origin: ExternalMessageType::Matrix(e.sender.to_string()),
-            // A message UID is not relevant regarding a live
-            // message listener. The Matrix SDK handles
-            // synchronization.
-            id: 0u32.into(),
-            timestamp: Timestamp::now(),
-            values: vec![text.body.into()],
-        };
-
-        match ctx.db.verify_message(&msg).await {
-            Err(e) => {
-                info!("Message verification failed: {:}", e);
-            }
-            _ => {}
-        }
-    }
-}
-
-async fn execute_command<'a>(db: &'a Database, command: Command) -> Response {
-    let local = |db: &'a Database, command: Command| async move {
-        match command {
-            Command::Status(addr) => {
-                let context = create_identity_context(addr);
-                let state = db.fetch_judgement_state(&context).await?;
-
-                // Determine response based on database lookup.
-                match state {
-                    Some(state) => Ok(Response::Status(state.into())),
-                    None => Ok(Response::IdentityNotFound),
-                }
-            }
-            Command::Verify(addr, fields) => {
-                let context = create_identity_context(addr.clone());
-
-                // Check if _all_ should be verified (respectively the full identity)
-                if fields.iter().any(|f| matches!(f, RawFieldName::All)) {
-                    return if db.full_manual_verification(&context).await? {
-                        Ok(Response::FullyVerified(addr))
-                    } else {
-                        Ok(Response::IdentityNotFound)
-                    }
-                }
-
-                // Verify each passed on field.
-                for field in &fields {
-                    if db
-                        .verify_manually(&context, field, true, None)
-                        .await?
-                        .is_none()
-                    {
-                        return Ok(Response::IdentityNotFound);
-                    }
-                }
-
-                Ok(Response::Verified(addr, fields))
-            }
-            Command::Help => Ok(Response::Help),
-        }
-    };
-
-    let res: crate::Result<Response> = local(db, command).await;
-    match res {
-        Ok(resp) => resp,
-        Err(err) => {
-            error!("Admin tool: {:?}", err);
-            dbg!(err);
-            Response::InternalError
-        }
-    }
-}
-
-/// Convenience function for creating a full identity context when only the
-/// address itself is present. Only supports Kusama and Polkadot for now.
-fn create_identity_context(address: ChainAddress) -> IdentityContext {
-    let chain = if address.as_str().starts_with('1') {
-        ChainName::Polkadot
-    } else {
-        ChainName::Kusama
-    };
-    IdentityContext { address, chain }
-}
-
-//------------------------------------------------------------------------------
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-enum Command {
-    Status(ChainAddress),
-    Verify(ChainAddress, Vec<RawFieldName>),
-    Help,
-}
-
-impl FromStr for Command {
-    type Err = &'static str;
-
-    // TODO: Refactor parsing.
-    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        let s = s.trim().replace("  ", " ");
-
-        if s.starts_with("status") {
-            let parts: Vec<&str> = s.split(' ').skip(1).collect();
-            if parts.len() != 1 {
-                return Err("Invalid `status` command");
-            }
-
-            Ok(Command::Status(ChainAddress::from(parts[0].to_string())))
-        } else if s.starts_with("verify") {
-            let parts: Vec<&str> = s.split(' ').skip(1).collect();
-            if parts.len() < 2 {
-                return Err("Invalid `verify` command");
-            }
-
-            Ok(Command::Verify(
-                ChainAddress::from(parts[0].to_string()),
-                parts[1..]
-                    .iter()
-                    .map(|s| RawFieldName::from_str(s))
-                    .collect::<std::result::Result<Vec<RawFieldName>, &'static str>>()?,
-            ))
-        } else if s.starts_with("help") {
-            let count = s.split(' ').count();
-
-            if count > 1 {
-                return Err("Invalid `help` command");
-            }
-
-            Ok(Command::Help)
-        } else {
-            Err("Unknown command")
-        }
-    }
 }
